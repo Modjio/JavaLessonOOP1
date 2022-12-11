d61a3eea7fa891bf22ecce452720bd0b2b45044c
Solodyankin Konstantin <Solodyankin.k@gmail.com>
2022-12-11 22:00:43 +0500

    hotfix TestStock, add reset color

--- src/TestStock.java
+++ src/TestStock.java
@@ -1,6 +1,7 @@
 public class TestStock {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
 
@@ -11,10 +12,10 @@ public class TestStock {
          */
         if (stock.currentPrice > stock.previousClosingPrice) {
             System.out.println("Акция: " + stock.symbol + " - " + stock.name + " " + stock.previousClosingPrice +
-                    " -> " + stock.currentPrice + " " + ANSI_GREEN + (int) (stock.getPercent() * 100) / 100.0 + "%");
+                    " -> " + stock.currentPrice + " " + ANSI_GREEN + (int) (stock.getPercent() * 100) / 100.0 + "%" + ANSI_RESET);
         } else {
             System.out.println("Акция: " + stock.symbol + " - " + stock.name + " " + stock.previousClosingPrice +
-                    " -> " + stock.currentPrice + " " + ANSI_RED + (int) (stock.getPercent() * 100) / 100.0 + "%");
+                    " -> " + stock.currentPrice + " " + ANSI_RED + (int) (stock.getPercent() * 100) / 100.0 + "%" + ANSI_RESET);
         }
 
     }

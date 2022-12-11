4501266311961b96f251377b2c77687708aa2375
Solodyankin Konstantin <Solodyankin.k@gmail.com>
2022-12-12 00:41:10 +0500

    update RandomGenerator determinate method

--- src/RandomGenerator.java
+++ src/RandomGenerator.java
@@ -31,5 +31,27 @@ public class RandomGenerator {
             System.out.print(generator2.nextInt(1000) + " ");
         }
 
+        /*
+         * other randomize type
+         */
+
+        Random generator3 = new Random(10);
+        System.out.print("\nДругие виды:");
+        System.out.print("\nСлучайное значение типа int: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(generator2.nextInt() + " ");
+        }
+        System.out.print("\nСлучайное значение от 0 до n: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(generator3.nextInt(10) + " ");
+        }
+        System.out.print("\nСлучайное значение типа long: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(generator2.nextLong() + " ");
+        }
+        System.out.print("\nСлучайное значение типа boolean: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(generator3.nextBoolean() + " ");
+        }
     }
 }
